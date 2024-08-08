bb4a97c85e36d0897982cfb1e41815fcdd32b929
Muzaffar Ali <muzafarali.cms@gmail.com>
2024-08-09 00:54:10 +0500

    Add files via upload

---
+++ 02 - Program for Plus (Muzaffar Ali)/02 - Program for Plus (Muzaffar Ali).py
@@ -0,0 +1,11 @@
+
+# Define the numbers
+Number1 = 180
+Number2 = 90
+Number3 = 96
+
+# Calculate the sum
+Answer = Number1+Number2+Number3
+
+# Print the result
+print ("Sum of", Number1, "and", Number2, "and", Number3, "is", Answer)
